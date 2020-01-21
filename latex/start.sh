7f8d7e9098171a8a49575e9de6ad8110350c40cf
Lafolia <violgdae@gmail.com>
2020-01-22 00:46:52 +0900

    start.sh を更新

--- latex/start.sh
+++ latex/start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-docker exec latex bash -c "latexmk -pvc /work/${1}" 2>&1 | egrep -A 2 -B 2 "up-to-date|^l."
-
+if [ -f ./${1} ]; then
+  docker run --rm --name latex -itd -v "$(pwd):/work" latex bash &&
+  docker exec latex bash -c "latexmk -pvc /work/${1}" 2>&1 | egrep -A 2 -B 2 "up-to-date|^l." &
+  echo "success"
+else
+  echo "No such file" 1>&2
+fi
