8ba2a873f2d934ceea6ee1498e18d3d775f3b892
Lafolia13 <violgdae@gmail.com>
2020-07-16 15:37:33 +0900

    move /.latexmkrc to ~/.latexmkrc, move workdir / to /work

--- latex/start.sh
+++ latex/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -f ./${1} ]; then
-  docker run --rm --name latex -itd -v "$(pwd):/work" latex bash &&
+  docker run --rm --name latex -itd -v "$(pwd):/work" -w "/work" latex bash &&
   docker exec latex bash -c "latexmk -pvc /work/${1}" 2>&1 | egrep -A 2 -B 2 "up-to-date|^l." &
   echo "success"
 else
