71f4079ca5d9d370c1b292b0443c7150a1b2b700
Lafolia13 <violgdae@gmail.com>
2020-10-04 19:34:26 +0900

    ディレクトリ内に .latexmkrc が存在する場合によしなにする

--- latex/start.sh
+++ latex/start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 if [ -f ./${1} ]; then
-  docker run --rm --name latex -itd -v "$(pwd):/work" -w "/work" latex bash &&
-  docker exec latex bash -c "latexmk -pvc /work/${1}" 2>&1 | egrep -A 2 -B 2 "up-to-date|^l." &
+  dir=$(dirname ${1})
+  base=$(basename ${1})
+  docker run --rm --name latex -itd -v "$(pwd):/work" -w "/work/${dir}" test_latex bash &&
+  docker exec latex bash -c "if [ -f .latexmkrc ]; then cp .latexmkrc ~/; fi && latexmk -pvc ${base}" 2>&1 | egrep -A 2 -B 2 "up-to-date|^l." &
   echo "success"
 else
   echo "No such file" 1>&2
